2c952cebd71f4e5aeb04ed91a3eb27854ced7cff
Tommy-Geenexus <tomgapplicationsdevelopment@gmail.com>
2023-01-14 15:56:55 +0100

    Apply spotless

--- app/src/main/java/com/none/tom/exiferaser/savepath/business/SavePathState.kt
+++ app/src/main/java/com/none/tom/exiferaser/savepath/business/SavePathState.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2021, Tom Geiselmann (tomgapplicationsdevelopment@gmail.com)
+ * Copyright (c) 2018-2023, Tom Geiselmann (tomgapplicationsdevelopment@gmail.com)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
  * and associated documentation files (the "Software"), to deal in the Software without restriction,
